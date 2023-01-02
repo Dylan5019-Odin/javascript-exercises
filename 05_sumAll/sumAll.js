8d0ed1f6f5816d8aa2628565f08538c4e828b4aa
Dylan5019-Odin <84342823+Dylan5019-Odin@users.noreply.github.com>
2023-01-02 13:21:16 -0600

    Finished Javascript exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
 
+    if (x < 0 || y < 0  || typeof x != 'number' || typeof y != 'number') {
+        return 'ERROR';
+    }
+
+
+    let sum = 0;
+    let max = x > y ? x : y;
+    let min = x < y ? x : y;
+    let range = max - min + 1;
+
+    for (let i = 0; i < range; i++) {
+        sum+= (max - i);
+       }
+    return sum;
 };
 
 // Do not edit below this line
