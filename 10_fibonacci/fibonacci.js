f8f1940183b6f5d30de1b28a976dc68d037c3205
Dylan5019-Odin <84342823+Dylan5019-Odin@users.noreply.github.com>
2023-01-02 19:17:21 -0600

    my solution to javascript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
 
+    if (x<0) return 'OOPS';
+
+    if (typeof x=='string') x = parseInt(x);
+
+    const fib = (n) => n < 2 ? n : fib(n-1) + fib(n-2);
+    return fib(x);
 };
 
 // Do not edit below this line
