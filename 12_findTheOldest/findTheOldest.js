dc34dd19bdcf499eb517e073183598d397a53368
Dylan5019-Odin <84342823+Dylan5019-Odin@users.noreply.github.com>
2023-01-02 19:44:21 -0600

    Had to look at StackOverflow for this one

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+let findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
+  const oldestPerson = people.reduce((winner, person) => {
+    if (
+      (person.yearOfDeath || currentYear) - person.yearOfBirth >
+      (winner.yearOfDeath || currentYear) - winner.yearOfBirth
+    ) {
+      return person;
+    } else if (
+      (person.yearOfDeath || currentYear) - person.yearOfBirth <=
+      (winner.yearOfDeath || currentYear) - winner.yearOfBirth
+    ) {
+      return winner;
+    }
+  });
 
+  return oldestPerson;
 };
 
 // Do not edit below this line
